9000af2e8910728050ec0942b7821c9f0821c592
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-05-02 16:35:10 +0100

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,21 +12,21 @@
  */
 int _isnumber(char *s)
 {
-int i, check, d;
+	int i, check, d;
 
-i = 0, d = 0, check = 1;
-if (*s == '-')
-i++;
-for (; *(s + i) != 0; i++)
-{
-d = isdigit(*(s + i));
-if (d == 0)
-{
-check = 0;
-break;
-}
-}
-return (check);
+	i = 0, d = 0, check = 1;
+	if (*s == '-')
+		i++;
+	for (; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
 }
 /**
  * main - Entry point
@@ -37,36 +37,36 @@ return (check);
  */
 int main(int argc, char **argv)
 {
-int j, ex, coins, cents, d;
-int c[5] = {25, 10, 5, 2, 1};
+	int j, ex, coins, cents, d;
+	int c[5] = {25, 10, 5, 2, 1};
 
-ex = 1, j = 0, coins = 0;
-if (argc == 2)
-{
-if (_isnumber(argv[1]))
-{
-ex = 0, cents = atoi(argv[1]);
-if (cents >= 0)
-{
-while (cents != 0)
-{
-d = cents / c[j];
-if (d == 0)
-{
-j++;
-}
-else
-{
-coins += d;
-cents -= (d * c[j]);
-}
-}
-}
-}
-}
-if (ex == 0)
-printf("%i\n", coins);
-else
-printf("%s\n", "Error");
-return (ex);
+	ex = 1, j = 0, coins = 0;
+	if (argc == 2)
+	{
+		if (_isnumber(argv[1]))
+		{
+			ex = 0, cents = atoi(argv[1]);
+			if (cents >= 0)
+			{
+				while (cents != 0)
+				{
+					d = cents / c[j];
+					if (d == 0)
+					{
+						j++;
+					}
+					else
+					{
+						coins += d;
+						cents -= (d * c[j]);
+					}
+				}
+			}
+		}
+	}
+	if (ex == 0)
+		printf("%i\n", coins);
+	else
+		printf("%s\n", "Error");
+	return (ex);
 }
