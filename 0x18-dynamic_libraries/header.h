d4e27a068cef7e785dde44da49c1930a34c291c5
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-07-12 15:42:38 +0100

    100 operations header

---
+++ 0x18-dynamic_libraries/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y );
+int mod(int x, int y);
+
+#endif
